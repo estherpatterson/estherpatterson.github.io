4b507ea0564ffcdcd605539fe01f8111d6ee74f5
estherpatterson <estherpatt06@icloud.com>
2021-07-24 16:01:17 -0400

    init

---
+++ javascript/home.js
@@ -0,0 +1,12 @@
+
+document.getElementById("button-1").onclick = function () {
+  location.href = "./pages/about.html";
+};
+
+document.getElementById("button-2").onclick = function () {
+  alert("Hello world!");
+};
+
+document.getElementById("button-3").onclick = function () {
+  document.getElementById("content").style.color = "blue";
+};
